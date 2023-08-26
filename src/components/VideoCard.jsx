288c3ea2b5d5c52730b8292c0fc02ee835860e7e
Balaguru <balagurusiva2002@gmail.com>
2023-08-26 12:07:50 +0530

    new file:   .vscode/settings.json 	modified:   src/components/ChannelCard.jsx 	modified:   src/components/VideoCard.jsx 	modified:   src/utils/constant.js

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,21 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Typography, Card, CardMedia, CardContent } from '@mui/material'
-import { CheckCircleOutline } from '@mui/icons-material'
+import { CheckCircleOutline, TypeSpecimen } from '@mui/icons-material'
 
 import { demoThumbnailUrl, demoChannelTitle, demoVideoTitle, demoVideoUrl, demoChannelUrl } from '../utils/constant'
 
 const VideoCard = ({video :{id: {videoId},snippet}}) => {
   
   return (
-     <Card>
+     <Card sx={{width:{md:'320px', xs:'100%'},boxShadow:'none', borderRadius:0}}>
         <Link to = {videoId ? `/video/${videoId}` : demoVideoUrl}>
-              <CardMedia 
-                   image={snippet?.thumbnails?.high?.url}
-                   alt={snippet?.title}
-                   sx={{width:358, height:180}}
-               />
+            <CardMedia image={snippet?.thumbnails?.high?.url} alt={snippet?.title} sx={{width:358, height:180}}/>
         </Link>
+        <CardContent sx={{backgroundColor:'#000', height:'110px'}}>
+               <Link to={videoId? `/video/${videoId}` : demoVideoUrl}>
+                  <Typography  varient="subtitle1" fontWeight="bold" color="#fff">
+                     {snippet?.title.slice(0,60) || demoVideoTitle.slice(0,60)}
+                  </Typography>
+                </Link>
+               <Link to ={snippet?.channelId ? `/channel/${snippet?.chennalId}` : demoChannelUrl}>
+                  <Typography varient="subtitle2" color='gray' fontWeight='bold'>
+                     {snippet?.chennalTitle || demoChannelTitle}
+                     <CheckCircleOutline  sx={{FontSize:12, color:'gray', ml:'5px',mb:'-5px'}}/>
+                  </Typography>
+               </Link>
+        </CardContent>
 
      </Card>
   )
