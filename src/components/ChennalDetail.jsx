96811b001ee116d91d349e649ea0bbd710f0548d
Balaguru <balagurusiva2002@gmail.com>
2023-09-04 07:15:20 +0530

    modified:   src/components/ChennalDetail.jsx

--- src/components/ChennalDetail.jsx
+++ src/components/ChennalDetail.jsx
@@ -24,7 +24,7 @@ const ChannelDetail = () => {
     <Box minHeight="95vh">
       <Box>
         <div style={{
-          height:'300px',
+          height:'150px',
           background: 'linear-gradient(90deg, rgba(0,148,247,1) 0%, rgba(206,100,284,1) 100%, rgba(0,212,255,1) 100%)',
           zIndex: 10,
         }} />
